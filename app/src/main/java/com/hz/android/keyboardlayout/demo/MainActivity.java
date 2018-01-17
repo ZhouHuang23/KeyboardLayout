f727336ec745ea0aff929c614ae55b20d4db0b7b
黄周 <384486632@qq.com>
2018-01-17 15:57:55 +0800

    first commit

---
+++ app/src/main/java/com/hz/android/keyboardlayout/demo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hz.android.keyboardlayout.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hz.android.keyboardlayout.KeyboardLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        KeyboardLayout keyboardLayout = (KeyboardLayout) findViewById(R.id.keyboard_layout);
+        keyboardLayout.setKeyboardLayoutListener(new KeyboardLayout.KeyboardLayoutListener() {
+            @Override
+            public void onKeyboardStateChanged(boolean isActive, int keyboardHeight) {
+                if (isActive) { //软键盘弹起
+                    Toast.makeText(MainActivity.this, "keyboardHeight =" + keyboardHeight, Toast.LENGTH_SHORT).show();
+                } else { //软键盘隐藏
+                    Toast.makeText(MainActivity.this, "软键盘隐藏" + keyboardHeight, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ library/src/main/java/com/hz/android/keyboardlayout/KeyboardLayout.java
@@ -0,0 +1,96 @@
+package com.hz.android.keyboardlayout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+/**
+ * 自定义布局
+ * Created by Administrator on 2018/1/15.
+ */
+
+public class KeyboardLayout extends FrameLayout {
+
+    private boolean isKeyboardActive = false;//是否激活输入法
+    private int keyboardHeight = 0;//输入法高度
+    private KeyboardLayoutListener keyboardLayoutListener;
+
+    public KeyboardLayout(Context context) {
+        this(context, null, 0);
+    }
+
+    public KeyboardLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public KeyboardLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //监听整个视图树布局变化
+        getViewTreeObserver().addOnGlobalLayoutListener(new KeyboardOnGlobalLayoutListener()); //监听布局改变
+    }
+
+    private class KeyboardOnGlobalLayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
+
+        int screenHeight = 0;
+
+        @Override
+        public void onGlobalLayout() {
+            //获取当前窗口的显示范围
+            Rect rect = new Rect();
+            ((Activity) getContext()).getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+
+            //计算得到弹出的高度
+            int screenHeight = getScreenHeight();
+            int popUpHeight = screenHeight - rect.bottom;
+
+            boolean isActive = false;
+            if (Math.abs(popUpHeight) > (screenHeight / 5)) {
+                isActive = true;
+                keyboardHeight = popUpHeight;
+            }
+            isKeyboardActive = isActive;
+
+            if (keyboardLayoutListener != null) { //通知外面
+                keyboardLayoutListener.onKeyboardStateChanged(isKeyboardActive, keyboardHeight);
+            }
+
+        }
+
+        private int getScreenHeight() {
+            if (screenHeight > 0) {
+                return screenHeight;
+            }
+
+            WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+            screenHeight = windowManager.getDefaultDisplay().getHeight();
+            return screenHeight;
+        }
+    }
+
+    /**
+     * @return 获取的输入法高度
+     */
+    public int getKeyboardHeight() {
+        return keyboardHeight;
+    }
+
+    public void setKeyboardLayoutListener(KeyboardLayoutListener keyboardLayoutListener) {
+        this.keyboardLayoutListener = keyboardLayoutListener;
+    }
+
+    public KeyboardLayoutListener getKeyboardLayoutListener() {
+        return keyboardLayoutListener;
+    }
+
+    public boolean isKeyboardActive() {
+        return isKeyboardActive;
+    }
+
+    public interface KeyboardLayoutListener {
+        void onKeyboardStateChanged(boolean isActive, int keyboardHeight);
+    }
+}
